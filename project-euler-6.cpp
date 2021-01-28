640e945d86d31b8d8e6cce5ce6e5e993a8d49e63
aquaqamelob <61473367+aquaqamelob@users.noreply.github.com>
2021-01-28 13:20:27 +0100

    Update project-euler-6.cpp

--- project-euler-6.cpp
+++ project-euler-6.cpp
@@ -5,12 +5,11 @@ using namespace std;
 
 int getNumber(int x)
 {
-	int sumOne=0,sumTwo=0,getSum=0;
+	int sumOne=0,getSum=0;
 	for(int i=1;i<x;i++)
 	{
 		sumOne+=pow(i,2);
 		getSum+=i;
-		
 	}	
 	return pow(getSum,2)-sumOne;
 }
