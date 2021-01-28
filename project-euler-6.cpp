e78470a16d599000c0c8fcc2649b48bf89b0a13c
aquaqamelob <61473367+aquaqamelob@users.noreply.github.com>
2021-01-28 13:16:43 +0100

    Add files via upload

---
+++ project-euler-6.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int getNumber(int x)
+{
+	int sumOne=0,sumTwo=0,getSum=0;
+	for(int i=1;i<x;i++)
+	{
+		sumOne+=pow(i,2);
+		getSum+=i;
+		
+	}	
+	return pow(getSum,2)-sumOne;
+}
+
+
+
+int main()
+{
+	cout<<getNumber(101);
+	return 0;
+}
